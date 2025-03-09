4947079c459e8f0795b2c1daf22607c30ba9efc6
Yemi <yemie@Yemis-MacBook-Pro.local>
2025-03-09 17:28:31 +1100

    WIP - TDL

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,17 @@ ENV KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1
 ENV TZ="Australia/Sydney"
 
 USER root
+
+# Install curl if it's not already present.
+RUN microdnf install -y curl
+
+# Download and install gosu. <will be used later to switch to non-root user>
+RUN curl -L https://github.com/tianon/gosu/releases/download/1.16/gosu-amd64 > /usr/local/bin/gosu && \
+    chmod +x /usr/local/bin/gosu
+
+# Install nc (netcat)
+RUN microdnf install -y nc && microdnf clean all
+
 # Ensure Kafka has the right permissions
 RUN mkdir -p /var/lib/kafka/data && \
     chown -R 1000:1000 /var/lib/kafka/data
@@ -20,7 +31,7 @@ COPY start-kafka.sh /usr/local/bin/start-kafka.sh
 RUN chmod +x /usr/local/bin/start-kafka.sh
 
 # Switch to non-root user
-USER appuser
+# USER appuser
 
 # Entry point
 ENTRYPOINT ["/usr/local/bin/start-kafka.sh"]
--- start-kafka.sh
+++ start-kafka.sh
@@ -2,25 +2,35 @@
 
 set -e
 
+# If script is run as appuser, restart it with root permissions
+# if [ "$(id -u)" -ne 0 ]; then
+#     echo "Switching to root for setup..."
+#     exec gosu root "$0" "$@"
+# fi
+
+echo "Updating host file :: In Kafka, the controller address must bind to a network interface on the machine or pod"
+printf "%s\t%s\n" "${MY_POD_IP}" "${KUBERNETES_SERVICE_NAME}.${KUBERNETES_NAMESPACE}.svc.cluster.local" >> /etc/hosts
+cat /etc/hosts
+echo ""
+
 echo "Starting Kafka..."
 echo ""
 
 # Set default values if not provided
 export KAFKA_PROCESS_ROLES="broker,controller"
 export KAFKA_NODE_ID="${KAFKA_NODE_ID:-1}"
-export KAFKA_CONTROLLER_QUORUM_VOTERS="1@${MY_POD_NAME}:9094"
-export KAFKA_SECURITY_PROTOCOL_MAP="SSL:SSL,CONTROLLER:SSL"
+export KAFKA_CONTROLLER_QUORUM_VOTERS="1@${KUBERNETES_SERVICE_NAME}.${KUBERNETES_NAMESPACE}.svc.cluster.local:9094"
 export KAFKA_LISTENER_SECURITY_PROTOCOL_MAP="SSL:SSL,CONTROLLER:SSL"
 export KAFKA_INTER_BROKER_LISTENER_NAME="SSL"
 export KAFKA_CONTROLLER_LISTENER_NAMES="CONTROLLER"
 export KAFKA_AUTO_CREATE_TOPICS_ENABLE="true"
-export KAFKA_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM=""
-export KAFKA_AUTO_CREATE_TOPICS_ENABLE="false"
+export KAFKA_SSL_ENDPOINT_IDENTIFICATION_ALGORITHM="HTTPS"
+export KAFKA_AUTO_CREATE_TOPICS_ENABLE="true"
 export KAFKA_DELETE_TOPIC_ENABLE="false"
 
 # Set TLS settings dynamically
-export KAFKA_LISTENERS="SSL://0.0.0.0:9093,CONTROLLER://0.0.0.0:9094"
-export KAFKA_ADVERTISED_LISTENERS="SSL://${KUBERNETES_SERVICE_NAME}.${KUBERNETES_NAMESPACE}.svc.cluster.local:${CLUSTER_PORT}"
+export KAFKA_LISTENERS="SSL://${MY_POD_NAME}:9093,CONTROLLER://${MY_POD_NAME}:9094"
+export KAFKA_ADVERTISED_LISTENERS="SSL://${KUBERNETES_SERVICE_NAME}.${KUBERNETES_NAMESPACE}.svc.cluster.local:${BROKER_PORT}"
 export KAFKA_SSL_KEYSTORE_LOCATION="${KAFKA_CERTS}/${KUBERNETES_SERVICE_NAME}.${KUBERNETES_NAMESPACE}.jks"
 export KAFKA_SSL_KEYSTORE_FILENAME="${KUBERNETES_SERVICE_NAME}.${KUBERNETES_NAMESPACE}.jks"
 export KAFKA_SSL_TRUSTSTORE_LOCATION="${KAFKA_CERTS}/truststore.jks"
@@ -36,5 +46,8 @@ if [[ ! -f "$KAFKA_SSL_KEYSTORE_LOCATION" || ! -f "$KAFKA_SSL_TRUSTSTORE_LOCATIO
     exit 1
 fi
 
+echo "Setup completed. Switching to appuser..."
+echo ""
+
 # Start Kafka
-exec /etc/confluent/docker/run
+exec gosu appuser /etc/confluent/docker/run
